6db6228fc6837e8cf45b99328e7e2d6a62ad6cfc
xinshengqin <xinshengqin2014@gmail.com>
2015-04-19 16:42:05 -0700

    rewrite root-finding algorithm in mesh_calculator

--- myfoam_mesh_calculator.py
+++ myfoam_mesh_calculator.py
@@ -1,46 +1,54 @@
 #!/usr/bin/python
 
-def get_n(delta1,deltan,length,debug=False):
+################################################################################
+def get_n(delta1,deltan,length,debug=0):
     #given delta1 (size of 1st cell), deltan (size of last cell) and length of the domain
     #giving it a guess of n can accelerate the computation
     #return number of cells, n
     #deltan may be slightly different from given value
-    if debug == True:
+    if debug > 0:
         print "call get_n()"
 
     deltan = float(deltan)
     delta1 = float(delta1)
     length = float(length)
-#initial guess of n
-    n = length*2/(delta1+deltan)
+    n = length*2/(delta1+deltan)#initial guess of n
     r1 = deltan/delta1
-    #if deltan=delta1
-    if abs(deltan-delta1) < 1e-6:
+    if abs(deltan-delta1) < 1e-6:#deltan=delta1
         return int(round(n))#round n into a integer
     max_it = 100000
-    convergence_rule = 0.001
+    convergence_rule = 0.0000001
 
-    lhs = delta1*(1-r1**(n/(n-1)))/(1-r1**(1/(n-1)))
+    #newton-raphson iteration
+    #lhs = delta1*(1-r1**(n/(n-1)))/(1-r1**(1/(n-1)))
+    #solve for f =  a*(1-b^(x/(x-1)))/(1-b^(1/(x-1)))-L = 0
+    f =  delta1*(1-r1**(n/(n-1))) / (1-r1**(1/(n-1)))- length
+    from math import log
     for i in range(max_it):
-        residual = (length-lhs)/length
-        #add upper and lower bound for residual in case the iteration oscillate too much
-        if residual > 0.1:
-            residual = 0.1
-        if residual < -0.1:
-            residual = -0.1
-        #print "residual:",residual
-        #print "n=",n
-        if abs(residual) < convergence_rule:
+        if debug > 0:
+            print "iteration: ",i
+            print "n:",n
+            print "residual:",f
+            print "\n"
+        if abs(f) < convergence_rule:
             break
-        n=(1+residual)*n
-        lhs = delta1*(1-r1**(n/(n-1)))/(1-r1**(1/(n-1)))
+        #f_prime got from wolfram alpha
+        f_prime = ( (delta1*  log(r1) )*(r1**(n/(n-1))-r1**(1/(n-1))) ) / ((n-1)**2 * (r1**(1/(n-1))-1)**2)
+        n = n - f/f_prime #new n
+        f =  delta1*(1-r1**(n/(n-1)))/(1-r1**(1/(n-1)))- length # new f
+
+    if debug > 0 :
+        print "total number of iteration for getting n:",i
+        print "residual:",abs(f)
+        print "n:",n
 
     return int(round(n))#round n into a integer
 
-def get_r_given_delta1(delta1,length,n,debug=False):
+################################################################################
+def get_r_given_delta1(delta1,length,n,debug=0):
     #return ratio between two adjacent cells, r = delta_{i+1} / delta_{i}
     #solve delta1*(1-r^n)/(1-r) = length to get r
-    if debug == True:
+    if debug > 0 :
         print "\n"*3
         print "Given delta1, length of edge, number of cells along this edge"
         print "call get_r_given_delta1"
@@ -48,36 +56,36 @@ def get_r_given_delta1(delta1,length,n,debug=False):
     delta1 = float(delta1)
     length = float(length)
     n = float(n)
-    max_it = 100000
-    convergence_rule = 0.0001
+    max_it = 2000
+    convergence_rule = 0.0000001
     if length/n > delta1:#cell size is increasing from left to right 
         increase = True
     else:
         increase = False
     r = (length/n/delta1)**(1/(n-1))#initial value for r
     if abs(r - 1) < 1e-6:#uniform grid
-        if debug == True:
+        if debug > 0 :
             print "Warning! The inputs indicates a uniform grid!"
         i=0
-    else:
-        lhs = delta1*(1-r**n)/(1-r)
+    else:#nonuniform grid
+        #newton-raphson iteration
+        #solve for f(r) = (1-r^n)/(1-r) - L/delta1 = 0
+        f = (1-r**n)/(1-r) - length/delta1 #f0
         for i in range(max_it):
-            residual = (length-lhs)/length
-            #add upper and lower bound for residual in case the iteration oscillate too much
-            if residual > 0.1:
-                residual = 0.1
-            if residual < -0.1:
-                residual = -0.1
-            #print "iteration:",i+1
-            #print "r=",r
-            #print "residual:",residual
-            if abs(residual) < convergence_rule:
+            if debug == 2:
+                print "iteration:",i
+                print "r=",r
+                print "residual:",f
+            if abs(f) < convergence_rule:
                 break
-            r=(1+residual)*r
-            lhs = delta1*(1-r**n)/(1-r)
+            f_prime = ((n-1)*r**(n+1)-n*r**n+r)/((1-r)**2*r)
+            r = r - f/f_prime #new r
+            f = (1-r**n)/(1-r) - length/delta1 #new f
+        if i == max_it-1:
+            print "Warning!! Maximum iteration reached. Something was wrong!"
     clusterRatio=r**(n-1)#ratio of size of last cell to first cell, delta_n/delta_1 
 
-    if debug == True:
+    if debug > 0:
         print "#"*80
         print "result summary"
         print "iteration times:", i
@@ -90,40 +98,44 @@ def get_r_given_delta1(delta1,length,n,debug=False):
     return clusterRatio
        
 
-def get_r_given_deltan(deltan,length,n,debug=False):
-    if debug == True:
+################################################################################
+def get_r_given_deltan(deltan,length,n,debug=0):
+    if debug > 0:
         print "call get_r_given_deltan"
     #return ratio between two adjacent cells, r = delta_{i+1} / delta_{i}
     #solve deltan*(1-r^n)/((1-r)*r^{n-1}) = length to get r
-
-    delta1 = float(deltan)
+    deltan =  float(deltan)
     length = float(length)
     n = float(n)
     max_it = 10000 
-    convergence_rule = 0.001
+    convergence_rule = 0.0000001
     if length/n > deltan:#cell size is increasing from left to right 
         increase = False 
     else:
         increase = True 
+
     r = (deltan*n/length)**(1/(n-1))#initial value for r
-
-    lhs = deltan*(1-r**n)/(1-r)/r**(n-1)
-    for i in range(max_it):
-        residual = (length-lhs)/length
-        #add upper and lower bound for residual in case the iteration oscillate too much
-        if residual > 0.2:
-            residual = 0.2
-        if residual < -0.2:
-            residual = -0.2
-        #print "iteration:",i+1
-        #print "r=",r
-        #print "residual:",residual
-        if abs(residual) < convergence_rule:
-            break
-        r=(1-residual)*r
-        lhs = deltan*(1-r**n)/(1-r)/r**(n-1)
-
-    if debug == True:
+    if abs(r - 1) < 1e-6:#uniform grid
+        if debug > 0 :
+            print "Warning! The inputs indicates a uniform grid!"
+        i=0
+    else:#nonuniform grid
+        #newton-raphson iteration
+        #solve for f(r) = (1-r^n)/((1-r)*r^(n-1)) - L/deltan = 0
+        f = (1-r**n)/((1-r)*r**(n-1)) - length/deltan #f0
+        for i in range(max_it):
+            if debug == 2:
+                print "iteration:",i
+                print "r=",r
+                print "residual:",f
+            if abs(f) < convergence_rule:
+                break
+            f_prime = -1* (r**(-n)*(r**n-n*r+n-1)) / (1-r)**2
+            r = r - f/f_prime #new r
+            f = (1-r**n)/((1-r)*r**(n-1)) - length/deltan #new f
+        if i == max_it-1:
+            print "Warning!! Maximum iteration reached. Something was wrong!"
+    if debug > 0 :
         print "#"*80
         print "result summary"
         print "iteration times:", i
@@ -141,10 +153,10 @@ def get_r_given_deltan(deltan,length,n,debug=False):
 #delta_1 is size of first cell
 #delta_n is size of last cell
 #uncomment the lines below to compute
-#length=0.3
+#length=6
 #delta_1=0.02
 #delta_n=0.1
-#get_r_given_delta1(delta_1,length,get_n(delta_1,delta_n,length),debug=True)
+#get_r_given_delta1(delta_1,length,get_n(delta_1,delta_n,length,debug=1),debug=2)
 
 
 ################################################################################
@@ -163,7 +175,7 @@ def get_r_given_deltan(deltan,length,n,debug=False):
 #length is length of the edge
 #delta_n is size of last cell
 #n is total number of cells along this edge
-#length=100
-#delta_n=10
-#n=25
-#get_r_given_deltan(delta_n,length,n)
+length=6
+delta_n=0.1
+n=120
+get_r_given_deltan(delta_n,length,n,debug = 2)
